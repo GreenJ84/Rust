c4d19875de1a82566166b825a60ceb7f72aa6d9e
Jesse Greenough <jesse.greenough84@gmail.com>
2023-03-12 18:25:02 -0700

    Complete rustlings Primative Types

---
+++ rustlings/exercises/primitive_types/primitive_types2.rs
@@ -0,0 +1,28 @@
+// primitive_types2.rs
+// Fill in the rest of the line that has code missing!
+
+
+I AM NOT DONE
+
+fn main() {
+    // Characters (`char`)
+
+    // Note the _single_ quotes, these are different from the double quotes
+    let my_first_initial = 'C';
+    if my_first_initial.is_alphabetic() {
+        println!("Alphabetical!");
+    } else if my_first_initial.is_numeric() {
+        println!("Numerical!");
+    } else {
+        println!("Neither alphabetic nor numeric!");
+    }
+
+    let fire = '🔥';
+    if fire.is_alphabetic() {
+        println!("Alphabetical!");
+    } else if fire.is_numeric() {
+        println!("Numerical!");
+    } else {
+        println!("Neither alphabetic nor numeric!");
+    }
+}
